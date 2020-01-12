02ffbc12176d08c7955f07d39019795574ef7963
Rodrigo Torres <rodrigo_fbm@hotmail.com>
2020-01-12 15:55:22 -0300

    Updates

---
+++ precache-manifest.68434d87829101f5ede72a2fb553597f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "682c92ab9cd27c8ec4a091ce7600a09c",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "52c6b98eefb39d867250",
+    "url": "/monsters-rolodex/static/css/main.e5196789.chunk.css"
+  },
+  {
+    "revision": "1093a6240466003a5984",
+    "url": "/monsters-rolodex/static/js/2.3a82cee6.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.3a82cee6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "52c6b98eefb39d867250",
+    "url": "/monsters-rolodex/static/js/main.d6172293.chunk.js"
+  },
+  {
+    "revision": "fcc6c7087f08bfeac99a",
+    "url": "/monsters-rolodex/static/js/runtime-main.52149680.js"
+  }
+]);
